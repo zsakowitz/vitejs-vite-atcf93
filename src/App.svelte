c7df082c4e2fff7ad53999f1b2d2e6948279587e
zsakowitz <zsakowitz@gmail.com>
2022-10-13 15:48:53 -0700

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,24 @@
 <script>
-  let age
-  age = 14
+  let age = 14
 
-  function onClick() {
-    age = age + 1
+  function die() {
+    age = 0
   }
 
-  function reduceTheAge() {
-    age = 0
+  function getOlder() {
+    age += 1
   }
 </script>
 
 <h1>Hello</h1>
 
 <p>
-  I have a dog. {age}
+  You are {age} years old.
 </p>
 
-<h2>This is level 2 heading.</h2>
+<button on:click={die}>Die</button>
 
-<button on:click={reduceTheAge}>Reset</button>
-
-<button on:click={onClick}>Increase the age</button>
+<button on:click={getOlder}>Get older</button>
 
 <style>
   h1 {
